dd68c0ae2290e53a36de4d386bb27c7bd1bed990
David Urrego <daviduzago@gmail.com>
2023-03-12 16:29:06 -0500

    github and google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,24 @@
 import NextAuth from "next-auth/next"
 import Credentials from "next-auth/providers/credentials"
 import bcrypt from "bcrypt"
+
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
 import prismadb from "@/lib/prismadb"
 
 export default NextAuth({
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_iD || "",
+            clientSecret: process.env.GITHUB_SECRET || ""
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID || "",
+            clientSecret: process.env.GOOGLE_SECRET || ""
+        }),
         Credentials({
             id: "credentials",
             name: "Credentials",
@@ -41,6 +55,7 @@ export default NextAuth({
         signIn: "/auth",
     },
     debug: process.env.NODE_ENV === "development",
+    adapter: PrismaAdapter(prismadb),
     session: { strategy: "jwt" },
     jwt: {
         secret: process.env.NEXTAUTH_JWT_SECRET,
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import Input from "@/components/input"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/router"
 
+import {FcGoogle} from "react-icons/fc"
+import {FaGithub} from "react-icons/fa"
+
 const Auth = () => {
     const router = useRouter()
     const [email, setEmail] = React.useState("")
@@ -54,7 +57,9 @@ const Auth = () => {
                     <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:h-2/5 lg:max-w-md w-full rounded-sm">
                         <h2 className="text-white text-4xl mb-8 font-semibold">{variant === "login" ? "Sign in" : "Sign up"}</h2>
                         <div className="flex flex-col gap-4">
-                            {variant === "register" && <Input id="name" type="text" label="Name" onChange={(e: any) => setName(e.target.value)} value={name} />}
+                            {variant === "register" && (
+                                <Input id="name" type="text" label="Name" onChange={(e: any) => setName(e.target.value)} value={name} />
+                            )}
                             <Input id="email" type="email" label="Email" onChange={(e: any) => setEmail(e.target.value)} value={email} />
                             <Input
                                 id="password"
@@ -64,10 +69,24 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button onClick={variant === "login" ? login : register} className="bg-red-600 text-white py-3 mt-10 rounded-md w-full hover:bg-red-700 transition">
+                        <button
+                            onClick={variant === "login" ? login : register}
+                            className="bg-red-600 text-white py-3 mt-10 rounded-md w-full hover:bg-red-700 transition">
                             {variant === "login" ? "Sign in" : "Sign up"}
                         </button>
-                        <p className="text-neutral-500 text-center mt-12">
+                        <div className="flex flex-grow items-center gap-8 mt-8 justify-center">
+                            <div
+                                onClick={() => signIn("google", { callbackUrl: "/" })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+                            </div>
+                            <div
+                                onClick={() => signIn("github", { callbackUrl: "/" })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
+                        <p className="text-neutral-500 font-semibold text-center mt-12">
                             {variant === "login" ? "New to Netflix?" : "Already have an account?"}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                                 {variant === "login" ? "Sign up now." : "Sign in now."}
