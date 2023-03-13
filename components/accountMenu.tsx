6081b2c165ef40eecaab8d423149cdee42592e25
David Urrego <daviduzago@gmail.com>
2023-03-13 12:07:09 -0500

    fix account menu

--- components/accountMenu.tsx
+++ components/accountMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { signOut } from "next-auth/react"
+import useCurrentUser from "@/hooks/useCurrentUser"
 
 interface AccountMenuProps {
     visible?: boolean
@@ -8,6 +9,8 @@ interface AccountMenuProps {
 const AccountMenu: React.FC<AccountMenuProps> = (props) => {
     const { visible } = props
 
+    const { data: user } = useCurrentUser()
+
     if (!visible) return null
 
     return (
@@ -15,7 +18,7 @@ const AccountMenu: React.FC<AccountMenuProps> = (props) => {
             <div className="flex flex-col gap-3">
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img src="/images/default-red.png" alt="Avatar" className="w-8 rounded-md" />
-                    <p className="text-white text-sm group-hover/item:underline">Username</p>
+                    <p className="text-white text-sm group-hover/item:underline">{user?.name}</p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px" />
                 <div onClick={() => signOut} className="px-3 text-center text-white text-sm hover:underline">
