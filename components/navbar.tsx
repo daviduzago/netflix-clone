f2c421fa4f3d99d4452d2ade1a8c764a9444e3a5
David Urrego <daviduzago@gmail.com>
2023-03-12 22:42:03 -0500

    fix style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -37,10 +37,7 @@ const Navbar = () => {
 
     return (
         <nav className="w-full fixed z-40">
-            <div
-                className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
-                    showBackground ? "bg-zing-900 bg-opacity-90" : ""
-                }`}>
+            <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? "bg-zing-900/90" : ""}`}>
                 <img src="/images/logo.png" alt="Logo" className="h-4 lg:h-7" />
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
                     <NavbarItem label="Home" />
